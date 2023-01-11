8eb1f4e15af0e4815844d1bdff85c3c96d66e379
heym4rc <marcioreg77@gmail.com>
2023-01-11 20:28:31 -0300

    Section Portfolio + Swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,88 +1,94 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-    navToggle.addEventListener('click', function(){
-        navMenu.classList.toggle('show-menu')
-    })
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-    navClose.addEventListener('click', function(){
-        navMenu.classList.remove('show-menu')
-    })
+  navClose.addEventListener("click", function () {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav-link')
+const navLink = document.querySelectorAll(".nav-link");
 
 function linkAction() {
-    // When we click on each nav-link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+  // When we click on each nav-link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction)) 
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName('skills-content'),
-    skillsHeader = document.querySelectorAll('.skills-header')
+const skillsContent = document.getElementsByClassName("skills-content"),
+  skillsHeader = document.querySelectorAll(".skills-header");
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className
+  let itemClass = this.parentNode.className;
 
-    for (let i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills-content skills-close'
-    } if (itemClass === 'skills-content skills-close') {
-        this.parentNode.className = 'skills-content skills-open'
-    }
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills-content skills-close";
+  }
+  if (itemClass === "skills-content skills-close") {
+    this.parentNode.className = "skills-content skills-open";
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills)
-})
+  el.addEventListener("click", toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]')
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
-tabs.forEach(tab =>{
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target)
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('qualification-active')
-        })
-        target.classList.add('qualification-active')
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification-active");
+    });
+    target.classList.add("qualification-active");
 
-        tab.forEach(tab =>{
-            tab.classList.remove('qualification-active')
-        })
-        tab.classList.add('qualification-active')
-    })
-})
-
-/*==================== SERVICES MODAL ====================*/
-
+    tab.forEach((tab) => {
+      tab.classList.remove("qualification-active");
+    });
+    tab.classList.add("qualification-active");
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new swiper('.portfolio-container', {
+  cssMode: true,
+  loop: true,
 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl:  '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickAble: true,
+  },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
